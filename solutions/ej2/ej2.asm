012f706cea544a9d2c01eb24dd16af05f936e6f3
PabloGCH <pablochoconi@gmail.com>
2024-10-12 20:49:56 -0300

    Avances en ejercicio 2

--- solutions/ej2/ej2.asm
+++ solutions/ej2/ej2.asm
@@ -8,17 +8,17 @@ section .data
   msgID             db        "Ingrese el padrón del alumno: ", 0 
   msgBirthDate      db        "Ingrese la fecha de nacimiento del alumno en formato 'dd/mm/yyyy': ", 0
   result            db        "El alumno %s con padrón %s tiene %s años", 0
-  dateFormat        db        "%d/%d/%d", 0 ; Format for parsing date
-  timeFormat        db        "%d", 0 ; Format for parsing time
+  dateFormat        db        "%i/%i/%i", 0 ; Format for parsing date
+  integerStr        db        "%i", 0
 
 section .bss
   fullName      resb      50   ; 50 bytes para el nombre
   id            resb      6    ; 6 bytes para el padrón
-  date          resb      10   ; 10 bytes para la fecha
-  age           resb      3    ; 3 bytes para la edad
-  dayOfBirth    resb      2    ; 2 bytes para el día de nacimiento
-  monthOfBirth  resb      2    ; 2 bytes para el mes de nacimiento
-  yearOfBirth   resb      4    ; 4 bytes para el año de nacimiento
+  date          resb      20   ; 20 bytes para la fecha
+  age           resd      1    ; 3 bytes para la edad
+  dayOfBirth    resd      1    ; 4 bytes para el día de nacimiento
+  monthOfBirth  resd      1    ; 4 bytes para el mes de nacimiento
+  yearOfBirth   resd      1    ; 4 bytes para el año de nacimiento
 
 section .text
 
@@ -53,18 +53,15 @@ dateInput:
 parseDate:
   mov     rdi,    date
   mov     rsi,    dateFormat
-  mov     rdx,    dayOfBirth
-  mov     rcx,    monthOfBirth
-  mov     r8,     yearOfBirth
+  lea     rdx,    [dayOfBirth]
+  lea     rcx,    [monthOfBirth]
+  lea     r8,     [yearOfBirth]
   call    sscanf
 
+  mov     rdi,    integerStr
+  mov     rsi,    [monthOfBirth]
+  call    printf
 
-
-;resultOutput:
-;  mov     rdi,    result
-;  mov     rsi,    fullName 
-;  mov     rdx,    id
-;  call    printf
 
 end:
   add     rsp,    8   ; Restauro rsp a su valor original
