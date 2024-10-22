91cb922d69edeaab85f89270fea631e07a9c2632
pablogch <pablochoconi@gmail.com>
2024-10-22 18:46:09 -0300

    Avances ejercicio 5

--- solutions/ej5/ej5.asm
+++ solutions/ej5/ej5.asm
@@ -4,34 +4,35 @@ global main
 
 
 section .data
-  numbers               dw    0000 0000 0000 0101   ; 5
-                              1111 1111 1111 1100   ; -4
-                              0000 0000 0110 1001   ; 105
-                              1111 1111 1000 0010   ; -126
-                              0000 0001 1110 1110   ; 494
-                              1111 1100 0010 0000   ; -1984
-                              0000 0111 1011 0010   ; 1970
-                              1110 1101 1110 0000   ; -3552
-                              0000 1111 0110 1010   ; 3946
-                              1111 0000 0100 1000   ; -2864
-                              0001 0000 1010 0000   ; 4128
-                              1101 0100 0001 0001   ; -11055
-                              0000 0101 1011 0111   ; 1463
-                              1100 1001 0101 1001   ; -15575
-                              0000 0110 0110 0110   ; 1638
-                              1011 1111 1101 1011   ; -16453
-                              0000 1110 0010 1010   ; 1762
-                              1111 1111 1001 1100   ; -100
-                              0001 0111 0010 1010   ; 7442
-                              1110 1011 0001 0001   ; -4767
+  numbers      dw    0000000000000101b,   ; 5
+               dw    1111111111111100b,   ; -4
+               dw    0000000001101001b,   ; 105
+               dw    1111111110000010b,   ; -126
+               dw    0000000111101110b,   ; 494
+               dw    1111110000100000b,   ; -1984
+               dw    0000011110110010b,   ; 1970
+               dw    1110110111100000b,   ; -3552
+               dw    0000111101101010b,   ; 3946
+               dw    1111000001001000b,   ; -2864
+               dw    0001000010100000b,   ; 4128
+               dw    1101010000010001b,   ; -11055
+               dw    0000010110110111b,   ; 1463
+               dw    1100100101011001b,   ; -15575
+               dw    0000011001100110b,   ; 1638
+               dw    1011111111011011b,   ; -16453
+               dw    0000111000101010b,   ; 1762
+               dw    1111111110011100b,   ; -100
+               dw    0001011100101010b,   ; 7442
+               dw    1110101100010001b    ; -4767
 
   numberOfElements      dq    20
-  minNumber             dw    -32768                ; - 2 ^ (16 - 1)
-  maxNumber             dw    32767                 ; 2 ^ (16 - 1) - 1
+  minPosNumber             dw    -32768                ; - 2 ^ (16 - 1)
+  maxPosNumber             dw    32767                 ; 2 ^ (16 - 1) - 1
   counter               dq    0
-  msgMin                db    "El número mínimo es: %i", 10, 0
-  msgMax                db    "El número máximo es: %i", 10, 0
-  msgAverage            db    "El promedio es: %i", 10, 0
+  msgMin                db    "El número mínimo es: %hi", 10, 0   ; hi = 16 bits
+  msgMax                db    "El número máximo es: %hi", 10, 0
+  msgAverage            db    "El promedio es: %hi", 10, 0
+  msgNumber             db    "%hi", 10, 0
 
 
 section .bss
@@ -39,15 +40,35 @@ section .bss
   maxNumber           resq  1
   total               resq  1
   average             resq  1
+  currentNumber       resq  1
 
 
 section .text
 main:
   iterate:
+    ; Consigue el número actual
+    mov rax, qword[counter]
+    imul rax, rax, 2          ; Multiplica por 2 para obtener el offset (Corrimiento) (2 bytes por elemento)
+    mov rdx, numbers
+    add rdx, rax              ; Suma el offset a la dirección del vector
+
+    mov eax, [rdx]            ; Guarda el número actual
+    mov dword[currentNumber], eax
+
+    printArg msgNumber, currentNumber
+
+    ; Arma solución parcial
+    call addToTotal
+    call checkForMin
+    call checkForMax
+
     inc qword[counter]
     mov rax, qword[counter]
     cmp rax, [numberOfElements]
     jl  iterate
+
+  call calculateAverage
+  call printResults
   ret
 
 
