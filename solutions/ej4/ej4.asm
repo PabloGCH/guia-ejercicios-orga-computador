9a6582ba50b979516eed3fdb7a75611053156c2f
pablogch <pablochoconi@gmail.com>
2024-10-22 13:41:35 -0300

    Resuelto ejercicio 4

--- solutions/ej4/ej4.asm
+++ solutions/ej4/ej4.asm
@@ -3,46 +3,112 @@ extern printf
 %include "macros.asm"
 
 section .data
-  msg             db      "Ingrese un numero: ", 0
-  format          db      "%li", 0
-  msgInput        db      "Ingreso: %li", 10, 0
-  iterations      dq      0
+  msg               db      "Ingrese un numero: ", 0
+  format            db      "%li", 0
+  msgInput          db      "Ingreso: %li", 10, 0
+  counter           dq      0
+  auxCounter        dq      0
+  numberOfElements  dq      4
+  swapped           dq      0
+
 
 section .bss
-  buffer          resb    100
-  number          resq    1
-  numberVector    times 4 resq 1
+  buffer                  resb    100
+  number                  resq    1
+  numberVector            times   4 resq 1 ; IMPORTANTE: TIMES DEBE SER IGUAL A numberOfElements
+  currentNumberAddress    resq 1
+  nextNumberAddress       resq 1
+
 
 
 section .text
 
 main:
+  ;INGRESO DE DATOS
   iterate:
     read  msg, buffer, format, number
     mov   rax, [number]       ; Nuevo número
 
     mov   rdx, numberVector   ; Dirección del vector
-    mov   rcx, [iterations]   ; Iteración, indice del vector
+    mov   rcx, [counter]      ; Iteración, indice del vector
     imul  rcx, rcx, 8         ; Multiplicamos por 8 para obtener el offset (Corrimiento) (8 bytes por número)
     add   rdx, rcx            ; Sumamos el offset a la dirección del vector
     mov   [rdx], rax          ; Guardamos el número en el vector
     
-    inc qword[iterations]
-    cmp qword[iterations], 4
+    mov rax, [numberOfElements]
+    inc qword[counter]
+    cmp qword[counter], rax
     jl iterate
 
-  mov qword[iterations], 0
+  ;ORDENAMIENTO
+  mov qword[counter], 0
+  firstLoop:
+    mov qword[swapped], 0
+    mov qword[auxCounter], 0
+    secondLoop:
+      ;Logica de swap
+      ;--------------------------------
+      ; Si no se hay que intercambiar se saltea el swap
+      swap:
+        ;------- Consigue primer número ------------
+        mov   rdx, numberVector                   ; Dirección del vector    
+        mov   rcx, [auxCounter]                   ; Iteración, indice del vector
+        imul  rcx, rcx, 8                         ; Multiplicamos por 8 para obtener el offset (Corrimiento) (8 bytes por número)
+        add   rdx, rcx                            ; Sumamos el offset a la dirección del vector
+        ;--------------------------------
+        mov   rax, [rdx]                          ; Guardo el número actual
+        mov   qword[currentNumberAddress], rdx    ; Guardo la dirección del número actual
+        ;------- Consigue segundo número ------------
+        mov   rdx, numberVector                   ; Dirección del vector
+        mov   rcx, [auxCounter]                   ; Iteración, indice del vector
+        inc   rcx                                 ; Siguiente número
+        imul  rcx, rcx, 8                         ; Multiplicamos por 8 para obtener el offset (Corrimiento) (8 bytes por número)
+        add   rdx, rcx                            ; Sumamos el offset a la dirección del vector
+        ;--------------------------------
+        mov   rbx, [rdx]                          ; Guardo el número siguiente
+        mov   qword[nextNumberAddress], rdx       ; Guardo la dirección del número siguiente
+        ;------- Compara los números ------------
+        cmp   rax, rbx                            ; Compara los números
+        jge   endOfSecondLoopIteration            ; Si no se deben intercambiar, termina (Esta ordenando de mayor a menor)
+        ;------- Intercambia los números ------------
+        mov   qword[swapped],               1
+        mov   rdx, [currentNumberAddress]
+        mov   [rdx],  rbx
+        mov   rdx, [nextNumberAddress]
+        mov   [rdx],  rax
+      endOfSecondLoopIteration:
+        ;Si no termino el segundo loop, Continua
+        mov   rax, [numberOfElements]
+        dec   rax ; Decrementa rax ya que no queremos iterar con el ultimo valor (no tiene siguiente)
+        inc   qword[auxCounter]
+        cmp   qword[auxCounter], rax
+        jl    secondLoop
+    ;Si no se hizo ningun swap, termina
+    cmp qword[swapped], 0
+    je printAll
+    ;Si no termino el primer loop, Continua
+    mov rax, [numberOfElements]
+    inc qword[counter]
+    cmp qword[counter], rax
+    jl firstLoop
+    jmp printAll
+ 
+
   printAll:
-    mov   rdx, numberVector   ; Dirección del vector
-    mov   rcx, [iterations]   ; Iteración, indice del vector
-    imul  rcx, rcx, 8         ; Multiplicamos por 8 para obtener el offset (Corrimiento) (8 bytes por número)
-    add   rdx, rcx            ; Sumamos el offset a la dirección del vector
+    ;IMPRESION DE RESULTADOS
+    mov qword[counter], 0
+    printNext:
+      mov   rdx, numberVector   ; Dirección del vector
+      mov   rcx, [counter]   ; Iteración, indice del vector
+      imul  rcx, rcx, 8         ; Multiplicamos por 8 para obtener el offset (Corrimiento) (8 bytes por número)
+      add   rdx, rcx            ; Sumamos el offset a la dirección del vector
 
-    printArg msgInput, rdx
-
-    inc qword[iterations]
-    cmp qword[iterations], 4
-    jl printAll
+      printArg msgInput, rdx
+      
+      mov rax, [numberOfElements]
+      inc qword[counter]
+      cmp qword[counter], rax
+      jl printNext
   ret
     
     
